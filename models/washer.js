9d7a12b1f197402e7e14701868addcd9df909ef7
Dovan Sanjaya <dovansanjaya24@gmail.com>
2018-10-26 10:05:18 +0700

    dikit lagi deploy

--- models/washer.js
+++ models/washer.js
@@ -7,7 +7,17 @@ module.exports = (sequelize, DataTypes) => {
     username: DataTypes.STRING,
     password: DataTypes.STRING,
     salt: DataTypes.BLOB
-  }, {});
+  }, {
+    hooks: {
+      beforeSave(washer, options) {
+        let testRegex = /[-!@#$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/
+        console.log('==>', testRegex.test(washer.username), washer.username)
+        if (testRegex.test(washer.username)) {
+          throw new Error('Username must contain letter or number')
+        }
+      }
+    }
+  });
   Washer.associate = function(models) {
     Washer.hasMany(models.Transaction)
     // associations can be defined here
